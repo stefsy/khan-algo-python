6d60b26b348e7ff4ad91eeed71f1178ca54c2b5a
stefsy desktop <steffisy@gmail.com>
2015-05-24 00:58:58 +0800

    implemented some pep8

--- selectionsort.py
+++ selectionsort.py
@@ -1,36 +1,36 @@
 """
-Select the next smallest element 
+Select the next smallest element
 """
-
 import random
 
+
 def swap(array, firstIndex, secondIndex):
-	tempArr = array[:] # making a copy
-	tempArr[firstIndex], tempArr[secondIndex] = array[secondIndex],array[firstIndex]
-	return tempArr
+    tempArr = array[:]  # making a copy
+    tempArr[firstIndex], tempArr[secondIndex] = array[secondIndex], array[firstIndex]
+    return tempArr
+
 
 def indexOfMinimum(array, startIndex):
-	minValue = array[startIndex]
-	minIndex = startIndex 
-	# print minValue, minIndex
+    minValue = array[startIndex]
+    minIndex = startIndex 
 
-	for i in range(startIndex,len(array)):
+    for i in range(startIndex, len(array)):
 		if array[i] < minValue:
 			minValue = array[i]
 			minIndex = i
 
-	return minIndex
+    return minIndex
+
 
 def selectionSort(array):
 	arr = array[:]
 	for i in range(len(array)):
-		arr = swap(arr,i,indexOfMinimum(arr,i))
+		arr = swap(arr, i, indexOfMinimum(arr, i))
 	return arr
 
 
 if __name__ == '__main__':
-	a, b = random.randint(0,9),random.randint(0,9)
+	a, b = random.randint(0, 9), random.randint(0, 9)
 	# print swap(range(10),a,b), a, b
 	# print indexOfMinimum([493,3,5,2,25],0)
-	print selectionSort([1,5,4,2,4,493,3,2,25])
-
+	print selectionSort([1, 5, 4, 2, 4, 493, 3, 2, 25])
